d8e9bba7abcb484796437398512a05ca260e248d
scotth527 <scottyh527@gmail.com>
2020-10-18 13:51:26 -0400

    Post id for comments table

--- db/migrate/20201012204424_create_comments.rb
+++ db/migrate/20201012204424_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :post_id
+      t.references :post
 
       t.timestamps
     end
