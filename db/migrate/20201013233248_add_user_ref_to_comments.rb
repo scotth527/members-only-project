eb9718eb6927736813607a52db71e609ccfcfdad
scotth527 <scottyh527@gmail.com>
2020-10-18 13:38:06 -0400

    Fixing issue with foreign key in the comments table

--- db/migrate/20201013233248_add_user_ref_to_comments.rb
+++ db/migrate/20201013233248_add_user_ref_to_comments.rb
@@ -1,5 +1,5 @@
 class AddUserRefToComments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :comments, :user_id, foreign_key:true
+    add_column :comments, :user, foreign_key:true
   end
 end
